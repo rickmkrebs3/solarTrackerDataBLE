5e546d6ad4d577047cc7b49a7cf6d3545115155a
EisaAlsh <eya@pdx.edu>
2024-07-16 18:17:22 -0700

    Add files via upload

---
+++ esp32_Servo.ino
@@ -0,0 +1,32 @@
+//www.elegoo.com [2016.12.08]
+// Modified for Adafruit HUZZAH32 ESP32 Feather (5/2/2019)
+// Requires the following library to be installed:
+//   ESP32Servo
+
+#include <ESP32Servo.h>
+
+const int servoPin = 23; // Servo data pulse pin
+
+Servo myservo;  // create servo object to control a servo
+
+int pos = 0;    // variable to store the servo angle position
+
+void setup()
+{
+  myservo.attach(servoPin);  // assigns servo pin to the servo object
+}
+
+void loop()
+{
+  for (pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees in 1 deg steps
+  {
+    myservo.write(pos);               // tell servo to go to position in variable 'pos'
+    delay(15);                        // waits 15ms for the servo to reach the position
+  }
+
+  for (pos = 180; pos >= 0; pos -= 1) // goes from 180 degrees to 0 degrees
+  {
+    myservo.write(pos);               // tell servo to go to position in variable 'pos'
+    delay(15);                        // waits 15ms for the servo to reach the position
+  }
+}
