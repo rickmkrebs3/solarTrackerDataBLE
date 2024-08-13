9d5fbba07d253146bc400a97bf6e41afb86fe86c
natesjullie <sjullienathan@gmail.com>
2024-08-12 21:44:18 -0700

    Update Solar_Tracker_Main.ino 8.12.2024

--- Solar_Tracker_Main.ino
+++ Solar_Tracker_Main.ino
@@ -13,8 +13,9 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 #include <RTClib.h>
+#include <Adafruit_INA260.h>
 
-
+Adafruit_INA260 ina260;
 RTC_PCF8523 rtc;
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address 0x3F, 16 column and 2 rows
 DHT dht(DHTPIN, DHTTYPE);
@@ -22,51 +23,62 @@ DHT dht(DHTPIN, DHTTYPE);
 //Servo variables
 Servo servo1;
 Servo servo2;
-int pos = 0;    // variable to store the servo angle position
 int lightValue1;
 int lightValue2;
 int lightValue3;
 int lightValue4;
-int mappedValue1;
-int mappedValue2;
+int difference1;
+int difference2;
 int angle1 = 90;
 int angle2 = 90;
 
 void setup()
 {
   Serial.begin(9600);
-
   //---------------------------------------------------------------------------------------------
   //                Temperature & Humidity Sensor + Liquid Crystal I2C LCD Display
   //---------------------------------------------------------------------------------------------
   dht.begin();     // initialize the sensor
   lcd.init();      // initialize the lcd
   lcd.backlight(); // open the backlight 
-
-  /*
+  
   //---------------------------------------------------------------------------------------------
   //                Real Time Clock (RTC)
   //---------------------------------------------------------------------------------------------
+  /*
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
-
   if (!rtc.initialized() || rtc.lostPower()) {
     Serial.println("RTC is NOT initialized, let's set the time!");
     // Comment out the next line once the time is set.
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-
   rtc.start();
   */
+
   //---------------------------------------------------------------------------------------------
   //                Servo Motors(2) & Photoresistors(4)
   //---------------------------------------------------------------------------------------------
   servo1.attach(8);
-  servo2.attach(9);
+  servo2.attach(7);
   servo1.write(angle1);
   servo2.write(angle2);
+
+  //---------------------------------------------------------------------------------------------
+  //                Voltage, Current, Power Module
+  //---------------------------------------------------------------------------------------------
+  while (!Serial) {
+    delay(10); // Wait for the Serial Monitor to open
+  }
+  Serial.println("Adafruit INA260 Test");
+  // Initialize the INA260
+  if (!ina260.begin()) {
+    Serial.println("Couldn't find INA260 chip");
+    while (1);
+  }
+  Serial.println("Found INA260 chip");
 }
 
 
@@ -75,69 +87,27 @@ void loop()
   //---------------------------------------------------------------------------------------------
   //                Servo Motors(2) & Photoresistors(4)
   //---------------------------------------------------------------------------------------------
-  delay(10);
   lightValue1 = analogRead(A0);  // Read the value from the first photoresistor 
   lightValue2 = analogRead(A1);  // Read the value from the second photoresistor 
   lightValue3 = analogRead(A2);  // Read the value from the third photoresistor 
-  lightValue4 = analogRead(A3);  // Read the value from the fourth photoresistor 
-  
-  int difference1 = lightValue1 - lightValue2;
-  int difference2 = lightValue3 - lightValue4;
+  lightValue4 = analogRead(A3);  // Read the value from the fourth photoresistor   
+  difference1 = lightValue1 - lightValue2;
+  difference2 = lightValue3 - lightValue4;
 
   if (difference1 > 50) {
-    // LDR1 is getting more light, move servo to the left
     angle1 -= 3;
   } else if (difference1 < -50) {
-    // LDR2 is getting more light, move servo to the right
     angle1 += 3;
   }
-
   if (difference2 > 50) {
-    // LDR1 is getting more light, move servo to the left
     angle2 -= 3;
   } else if (difference2 < -50) {
-    // LDR2 is getting more light, move servo to the right
     angle2 += 3;
   }
-
   angle1 = constrain(angle1, 0, 180);
   angle2 = constrain(angle2, 0, 180);
-
   servo1.write(angle2);
   servo2.write(angle1);
-
-  // Map each light value to a range of 0-180 degrees
-  
-  /*
-  if (lightValue1 > lightValue2) {
-      int mappedValue1 = map(lightValue1, 0, 1023, 0, 89);
-
-      if (lightValue3 > lightValue4) {
-        int mappedValue2 = map(lightValue3, 0, 1023, 0, 89);
-      }
-      else if (lightValue3 < lightValue4) {
-        int mappedValue2 = map(lightValue4, 0, 1023, 90, 180);
-      }
-  }
-  else if (lightValue1 < lightValue2) {
-      int mappedValue1 = map(lightValue2, 0, 1023, 90, 180);
-
-      if (lightValue3 > lightValue4) {
-        int mappedValue2 = map(lightValue3, 0, 1023, 0, 89);
-      }
-      else if (lightValue3 < lightValue4) {
-        int mappedValue2 = map(lightValue4, 0, 1023, 90, 180);
-      }
-  }*/
-  
-  //int maxMappedValue1 = (mappedValue1 + mappedValue2) / 2;
-  //int maxMappedValue2 = (mappedValue3 + mappedValue4) / 2;
-  
-  //Serial.println("Analog  Value: ");
-  
-  //servo1.write(mappedValue1);
-  //servo2.write(mappedValue2);
-
   delay(10);
 
   //---------------------------------------------------------------------------------------------
@@ -160,8 +130,6 @@ void loop()
   //Serial.print(now.second(), DEC);
   //Serial.println();
 
-  // Wait for 1 second
-  //delay(1000);
 
 
   //---------------------------------------------------------------------------------------------
@@ -179,6 +147,7 @@ void loop()
   else {
     lcd.setCursor(0, 0);                                   // start to print at the first row
     //lcd.print("Time: ");                                   // 'Temp:' text displayed
+    //lcd.print("now")
     lcd.setCursor(0, 1);                                   // start to print at the second row
     lcd.print("T/H:");                               //'Humidity:' displayed
     lcd.print(tempF);                                      // print the temperature
@@ -187,7 +156,36 @@ void loop()
     lcd.print(humi);                                       // print the humidity
     lcd.print("%");                                        //'%' displayed
   }
+  delay(2000);
+  lcd.clear();
 
+  //---------------------------------------------------------------------------------------------
+  //                Voltage, Current, Power Module
+  //---------------------------------------------------------------------------------------------
+  // Read and print voltage
+  float bus_voltage = ina260.readBusVoltage();
+  float current = ina260.readCurrent();
+  float power = ina260.readPower();
   
+  Serial.print("Bus Voltage:   "); Serial.print(bus_voltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(-current); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(bus_voltage*(-current)); Serial.println(" mW");
   
+  power = bus_voltage * -current;
+
+  if (isnan(bus_voltage) || isnan(current) || isnan(power)) {                       // check if any reads failed
+    lcd.setCursor(0, 1);
+    lcd.print("Failed");
+  } 
+  else {
+    lcd.setCursor(0, 0);                                   // start to print at the first row
+    //lcd.print("Time: ");                                   // 'Temp:' text displayed
+    //lcd.print("now");
+    lcd.setCursor(0, 1);                                   // start to print at the second row
+    lcd.print("Power: ");                               //'Humidity:' displayed
+    lcd.print(power);                                      // print the temperature
+    lcd.print("mW");
+  }
+  delay(2000); // Wait for 2 seconds before the next reading
+  lcd.clear();
 }
